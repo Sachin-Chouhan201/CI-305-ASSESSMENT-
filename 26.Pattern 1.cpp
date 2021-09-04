3d50ea6add69c9d16bcc3ffafd255ac4c602549d
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2021-09-04 13:40:59 +0530

    Create 26.Pattern 1.cpp
    A program for print the series in the following format
    54321
    4321
    321
    21
    1

---
+++ 26.Pattern 1.cpp
@@ -0,0 +1,35 @@
+/*
+“A program for print the series in the following format”
+54321
+4321
+321
+21
+1
+*/
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+ int i, j;
+ for(i=5; i>=1; i--)
+ {
+  for(j=i; j>=1; j--)
+   cout << j;
+  cout << "\n"; 
+ }
+ return 0;
+}
+
+/*
+
+OUTPUT:-
+
+54321
+4321
+321
+21
+1
+
+*/
