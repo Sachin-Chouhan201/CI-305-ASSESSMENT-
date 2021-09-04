340c749524ca6717b61a880986c5a48fdb65285a
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2021-09-04 14:38:42 +0530

    Create 34.Printing characters base on their respective integer number(ASCII).cpp
    Program for Printing characters base on their respective integer number.

---
+++ 34.Printing characters base on their respective integer number(ASCII).cpp
@@ -0,0 +1,24 @@
+/*
+“Program for Printing characters base on their respective integer number.”
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char c;
+  cout<<"Enter The Charcter ";
+  cin>>c;
+	cout<<endl<<"The ASCII value of "<<c<<" is "<<int(c);
+	return 0;
+}
+
+/*
+I/P:-
+a
+
+OUTPUT:-
+Enter The Charcter 
+The ASCII value of a is 97
+
+*/
