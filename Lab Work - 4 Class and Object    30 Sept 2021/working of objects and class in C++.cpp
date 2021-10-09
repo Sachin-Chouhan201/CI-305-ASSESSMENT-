3ca4252e2ec6d2651f7b5d1b804a0da8222c554f
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2021-10-09 12:08:52 +0530

    Create working of objects and class in C++.cpp

---
+++ Lab Work - 4 Class and Object    30 Sept 2021/working of objects and class in C++.cpp
@@ -0,0 +1,25 @@
+// Program to illustrate the working of objects and class in C++ Programming
+#include <iostream>
+using namespace std;
+class Room {            // create a class
+   public:
+    double length;
+    double breadth;
+    double height;
+    double calculateArea() {
+        return length * breadth;
+    }
+    double calculateVolume() {
+        return length * breadth * height;
+    }
+};
+int main() {
+    Room room1; // create object of Room class
+    room1.length = 42.5; // assign values to data members
+    room1.breadth = 30.8;
+    room1.height = 19.2;
+    // calculate and display the area and volume of the room
+    cout<<"Area of Room =  "<<room1.calculateArea()<<endl;
+    cout<<"Volume of Room =  "<<room1.calculateVolume()<< endl;
+    return 0;
+}
