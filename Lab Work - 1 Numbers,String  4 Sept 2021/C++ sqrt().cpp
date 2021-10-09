80758f29ac752fe4c3ebd1506a373e26e04efc56
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2021-10-09 12:52:26 +0530

    Create C++ sqrt().cpp

---
+++ Lab Work - 1 Numbers,String  4 Sept 2021/C++ sqrt().cpp
@@ -0,0 +1,15 @@
+//C++ sqrt()
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  double num = 10.25;
+  double result = sqrt(num);
+  cout << "Square root of " << num << " is " << result<<"\n";
+  
+  long num2 = 464453422;
+  result= sqrt(num2);
+  cout << "Square root of " << num2 << " is " << result<<"\n";
+  return 0;
+}
