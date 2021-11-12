f17ae16da67b823e879c03308118ae35d02b74c8
Sachin-Chouhan201 <75372486+Sachin-Chouhan201@users.noreply.github.com>
2021-11-12 22:33:35 +0530

    Create C++ Program to Delete element from Array.cpp

---
+++ Lab Work - 2  Array  9 Sept 2021/C++ Program to Delete element from Array.cpp
@@ -0,0 +1,59 @@
+//C++ Program to Delete element from Array
+
+/*
+
+Delete Element from Array in C++
+The program takes user input for the size of an array and for the elements of an array. Then the program will ask the user to enter a number from the array to be deleted.
+
+Lastly, after the deletion of an element, the new array is displayed.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n\nWelcome to Studytonight :-)\n\n\n";
+    cout << " =====  Program to demonstrate Deletion of an element from an Array ===== \n\n";
+
+    int n;
+
+    cout << " Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n], i, pos;
+
+    cout << "\n\n Enter the " << n << " elements of the array: \n\n";
+    for(i=0; i<n; i++)
+    {
+        cin >> arr[i]; 
+    }
+
+    //Printing the original array before deletion
+    cout << "\n\n The " << n << " elements of the array, before deletion are : \n\n";
+    for(i=0; i<n; i++)
+    {
+        cout << arr[i] << "  "; 
+    }
+
+    cout << "\n\n Enter the position, between 1 and " << n << " , of the element to be deleted : ";
+    cin >> pos;
+
+    //Performing the deletion logic
+    --pos;//as index of the element to be deleted is 1 less than it's position
+    for (i = pos; i <= 9; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    cout << " \n\nThe " << n-1 << " elements of the array, after deletion are : \n\n";
+    for(i=0; i<n-1; i++)
+    {
+        cout << arr[i] << "  "; 
+    }
+             
+    cout << "\n\n";
+
+    return 0;
+}
